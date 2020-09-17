dce7f7eaa6ad12c33d0f42eced988576d0672e91
Maggoff <magmbov@gmail.com>
2020-09-17 13:24:39 +0300

    Зробив другий блок

--- js/main.js
+++ js/main.js
@@ -1,19 +1,35 @@
-let vsOpts = {
-  $slides: $('.main__slide'),
-  $list: $('.main__slides'),
-  duration: 6,
-  lineHeight: 74
-}
+$(document).ready(function () {
 
-let vSlide = new TimelineMax({
-  paused: true,
-  repeat: -1
-})
+  let vsOpts = {
+    $slides: $('.main__slide'),
+    $list: $('.main__slides'),
+    duration: 6,
+    lineHeight: 74
+  }
 
-vsOpts.$slides.each(function(i) {
-  vSlide.to(vsOpts.$list, vsOpts.duration / vsOpts.$slides.length, {
-    y: i * -1 * vsOpts.lineHeight,
-    ease: Elastic.easeOut.config(1, 0.7)
+  let vSlide = new TimelineMax({
+    paused: true,
+    repeat: -1
   })
-})
-vSlide.play()
+
+  vsOpts.$slides.each(function (i) {
+    vSlide.to(vsOpts.$list, vsOpts.duration / vsOpts.$slides.length, {
+      y: i * -1 * vsOpts.lineHeight,
+      ease: Elastic.easeOut.config(1, 0.7)
+    })
+  })
+  vSlide.play()
+
+  let onPause = false;
+
+  $('.callToAction__right__video').on('click', function () {
+    if(onPause===false){
+      document.getElementsByTagName("video")[0].pause();
+      onPause = true;
+    }else{
+      document.getElementsByTagName("video")[0].play();
+      onPause = false;
+    }
+  });
+
+});
