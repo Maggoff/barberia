ef853a7711edd9608a7d67f30e93194cb24302c9
Maggoff <magmbov@gmail.com>
2020-09-18 16:46:55 +0300

    Зробив блок бару, команди, стрижок і навчання

--- js/main.js
+++ js/main.js
@@ -31,11 +31,9 @@ $(document).ready(function () {
     id[i].onmouseover = function (e) {
       if (oldI != i) {
         oldI = i;
-        console.log('text');
         $('.services__info__img, .services__info__text').stop(true, true).animate({ opacity: 0 }, 800, function () {
           img.src = id[i].getAttribute("data-img");
           txt.innerHTML = id[i].getAttribute("data-text");
-          console.log('opacity');
         });
         $('.services__info__img, .services__info__text').stop(true, true).animate({ opacity: 1 }, 800,);
       }
@@ -54,4 +52,39 @@ $(document).ready(function () {
     //   $('.services__info__img').animate({ opacity: 1 }, 200)
     // }
   }
+
+  let idBar = document.getElementsByClassName("bar");
+  let imgBar = document.getElementById("bar__info__img");
+  let txtBar = document.getElementById("bar__info__text");
+  let oldIBar = 100;
+  for (let i = 0; i < idBar.length; i++) {
+    idBar[i].onmouseover = function (e) {
+      if (oldIBar != i) {
+        oldIBar = i;
+        $('.services__info__img, .services__info__text').stop(true, true).animate({ opacity: 0 }, 800, function () {
+          imgBar.src = idBar[i].getAttribute("data-img");
+          txtBar.innerHTML = idBar[i].getAttribute("data-text");
+        });
+        $('.services__info__img, .services__info__text').stop(true, true).animate({ opacity: 1 }, 800,);
+      }
+    }
+  }
+
+  let idHaircut = document.getElementsByClassName("haircut");
+  let imgHaircut = document.getElementById("haircuts__info__img");
+  let txtHaircut = document.getElementById("haircuts__info__text");
+  let oldIHaircut = 100;
+  for (let i = 0; i < idHaircut.length; i++) {
+    idHaircut[i].onmouseover = function (e) {
+      if (oldIHaircut != i) {
+        oldIHaircut = i;
+        $('.services__info__img, .services__info__text').stop(true, true).animate({ opacity: 0 }, 800, function () {
+          imgHaircut.src = idHaircut[i].getAttribute("data-img");
+          txtHaircut.innerHTML = idHaircut[i].getAttribute("data-text");
+        });
+        $('.services__info__img, .services__info__text').stop(true, true).animate({ opacity: 1 }, 800,);
+      }
+    }
+  }
+
 });
