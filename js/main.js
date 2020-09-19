e343390fd710fd02661b0b03e02d675152b070ab
Maggoff <magmbov@gmail.com>
2020-09-19 13:41:56 +0300

    Додав TwentyTwenty в Total Look

--- js/main.js
+++ js/main.js
@@ -117,4 +117,14 @@ $(document).ready(function () {
     }
   }
 
+  $(function () {
+    $("#totalLook__container").twentytwenty({
+      default_offset_pct: 0.5,
+      orientation: 'horizontal',
+      no_overlay: true,
+      move_with_handle_only: true,
+      click_to_move: false
+    });
+  });
+
 });
